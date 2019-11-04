88e8ed09711e237f065705d9d242eaac097c6ace
Alejandra Zambrano <alzambranolu@unal.edu.co>
2019-11-04 16:22:42 -0500

    v2

--- mobile-app/app/src/main/java/co/edu/unal/se1/Presentation/activity/MainActivity.java
+++ mobile-app/app/src/main/java/co/edu/unal/se1/Presentation/activity/MainActivity.java
@@ -1,76 +1,61 @@
 package co.edu.unal.se1.Presentation.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
-import com.google.android.material.textfield.TextInputEditText;
-
+import androidx.appcompat.app.AppCompatActivity;
 import co.edu.unal.se1.R;
 import co.edu.unal.se1.businessLogic.controller.*;
-import co.edu.unal.se1.dataAccess.model.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int idUser;
+
+    public void abrir_create( View view){
+        Intent intent = new Intent(this, crear_usuario.class );
+        startActivity(intent);
+    }
+
+    public void abrir_saldo (View view){
+        Intent intent = new Intent (this, ver_saldo.class);
+        startActivity(intent);
+
+    }
+
     private UserController userController;
     private AccountController accountController;
+
+
+    public boolean verificar (int id, String password){
+        UserController userController = new UserController();
+        return userController.verifyUser( id , password , getApplicationContext() );
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextInputEditText idInput = findViewById(R.id.id);
-        final TextInputEditText nameInput = findViewById(R.id.name);
-        final TextInputEditText balanceInput = findViewById(R.id.balance);
+        final EditText idInput = findViewById(R.id.userID);
+        final EditText passInput = findViewById(R.id.password);
 
-        Button createButton = findViewById(R.id.createButton);
-        createButton.setOnClickListener(new View.OnClickListener() {
+        Button login = findViewById(R.id.logIn);
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                User user = new User();
-                Account account = new Account();
-                user.setId(Integer.parseInt(idInput.getText().toString()));
-                account.setId(Integer.parseInt(idInput.getText().toString()));
-                user.setName(nameInput.getText().toString());
-                account.setBalance(Double.parseDouble(balanceInput.getText().toString()));
-
-                userController = new UserController();
-                userController.createUser(user, getApplicationContext());
-                accountController = new AccountController();
-                accountController.createAccount(account, getApplicationContext());
-            }
-        });
-
-        final TextView sourceIdInput = findViewById(R.id.sourceId);
-        final TextView targetIdInput = findViewById(R.id.targetId);
-        final TextView valueInput = findViewById(R.id.value);
-
-        Button sendMoneyButton = findViewById(R.id.sendMoneyButton);
-        sendMoneyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                userController = new UserController();
-                accountController = new AccountController();
-
-                int sourceId = Integer.parseInt(sourceIdInput.getText().toString());
-                int targetId = Integer.parseInt(targetIdInput.getText().toString());
-                double value = Double.parseDouble(valueInput.getText().toString());
-
-                boolean transaction = accountController.sendMoney(sourceId, targetId, value, getApplicationContext());
-
-                if (transaction) {
-                    System.out.println("¡Transacción satisfactoria!");
-                } else {
-                    System.out.println("¡Transacción no satisfactoria!");
+                final boolean verify = verificar( Integer.parseInt( idInput.getText().toString() ) , passInput.getText().toString() ) ;
+                if (verify){
+                    idUser = Integer.parseInt( idInput.getText().toString() );
+                    abrir_saldo(v);
                 }
             }
         });
+
     }
+
+
 
 }
--- mobile-app/app/src/main/java/co/edu/unal/se1/Presentation/activity/ver_saldo.java
+++ mobile-app/app/src/main/java/co/edu/unal/se1/Presentation/activity/ver_saldo.java
@@ -2,18 +2,11 @@ package co.edu.unal.se1.Presentation.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.audiofx.AcousticEchoCanceler;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import com.google.android.material.textfield.TextInputEditText;
 
 import co.edu.unal.se1.R;
 import co.edu.unal.se1.businessLogic.controller.*;
-import co.edu.unal.se1.dataAccess.model.*;
 
 public class ver_saldo extends AppCompatActivity {
 
@@ -23,7 +16,7 @@ public class ver_saldo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.logeado);
+        setContentView(R.layout.ver_saldo);
 
         final TextView nombre = findViewById(R.id.Nombre_text);
         final TextView cc = findViewById(R.id.cc_text);
@@ -32,9 +25,9 @@ public class ver_saldo extends AppCompatActivity {
         UserController userController = new UserController();
         AccountController accountController = new AccountController();
 
-        nombre.setText( userController.getNameById( new_main.idUser , getApplicationContext() ) );
-        cc.setText( new_main.idUser );
-        saldo.setText(String.valueOf( accountController.getBalanceById( new_main.idUser , getApplicationContext() ) ) );
+        nombre.setText( userController.getNameById( MainActivity.idUser , getApplicationContext() ) );
+        cc.setText( MainActivity.idUser );
+        saldo.setText(String.valueOf( accountController.getBalanceById( MainActivity.idUser , getApplicationContext() ) ) );
 
     }
 
