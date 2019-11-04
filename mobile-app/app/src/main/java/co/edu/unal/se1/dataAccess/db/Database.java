609ea93821b6f2546aaa6c56872bd70df8d1935b
Andres Ricardo Perez <riperezro@unal.edu.co>
2019-11-04 11:49:54 -0500

    Aux

--- mobile-app/app/src/main/java/co/edu/unal/se1/dataAccess/db/Database.java
+++ mobile-app/app/src/main/java/co/edu/unal/se1/dataAccess/db/Database.java
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase;
 import co.edu.unal.se1.dataAccess.dao.*;
 import co.edu.unal.se1.dataAccess.model.*;
 
-@androidx.room.Database(entities = {User.class,Account.class}, version = 2)
+@androidx.room.Database(entities = {User.class,Account.class}, version = 2 )
 public abstract class Database extends RoomDatabase {
 
     public abstract UserDao userDao();
