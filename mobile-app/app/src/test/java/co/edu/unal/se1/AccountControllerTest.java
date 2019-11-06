207ee1c9395de96834212cf9ca5a8e1ddb4d8281
Andres Ricardo Perez <riperezro@unal.edu.co>
2019-11-06 13:25:09 -0500

    Run Tests

--- mobile-app/app/src/test/java/co/edu/unal/se1/AccountControllerTest.java
+++ mobile-app/app/src/test/java/co/edu/unal/se1/AccountControllerTest.java
@@ -1,16 +1,23 @@
 package co.edu.unal.se1;
 
+import co.edu.unal.se1.businessLogic.controller.AccountController;
+import co.edu.unal.se1.dataAccess.model.Account;
+
 import android.content.Context;
 import androidx.test.core.app.ApplicationProvider;
 import org.junit.Test;
 
-import co.edu.unal.se1.businessLogic.controller.AccountController;
-import co.edu.unal.se1.businessLogic.controller.UserController;
-import co.edu.unal.se1.dataAccess.model.Account;
-
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 24)
 public class AccountControllerTest {
+
+    private Context context = ApplicationProvider.getApplicationContext();
+
     @Test
     public void getBalancebyId_withNewAccount(){
         AccountController accountController = new AccountController();
@@ -19,9 +26,9 @@ public class AccountControllerTest {
         Account account = new Account();
         account.setId( testId );
         account.setBalance( testBalance );
-        accountController.createAccount( account , ApplicationProvider.getApplicationContext() );
-        long result = accountController.getBalanceById( testId , ApplicationProvider.getApplicationContext() );
-        accountController.deleteAccount( testId , ApplicationProvider.getApplicationContext() );
+        accountController.createAccount( account , context );
+        long result = accountController.getBalanceById( testId , context );
+        accountController.deleteAccount( testId , context );
         assertEquals( testBalance , result );
     }
 }
